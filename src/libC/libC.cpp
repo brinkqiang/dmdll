4a7c65fd3371dd0c109102cf392b74e5e5605b7b
brinkqiang <brink.qiang@gmail.com>
2025-03-10 23:41:22 +0800

    add

--- src/libC/libC.cpp
+++ src/libC/libC.cpp
@@ -5,5 +5,5 @@ int g_globalVar = 0;
 void modifyGlobalVar(int value) {
     g_globalVar = value;
 
-    printf("Global variable is now: %d, %p", g_globalVar, &g_globalVar);
+    printf("Global variable is now: %d, %p\n", g_globalVar, &g_globalVar);
 }
