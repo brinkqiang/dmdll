e642af8b7167e6bc2da678f5d03b792c74030aad
brinkqiang <brink.qiang@gmail.com>
2025-03-10 23:36:07 +0800

    add

--- src/libC/libC.cpp
+++ src/libC/libC.cpp
@@ -1,7 +1,9 @@
 #include "libC.h"
-
+#include <stdio.h>
 int g_globalVar = 0;
 
 void modifyGlobalVar(int value) {
     g_globalVar = value;
+
+    printf("Global variable is now: %d, %p", g_globalVar, &g_globalVar);
 }
--- test/dmdlltest/dmdlltest.cpp
+++ test/dmdlltest/dmdlltest.cpp
@@ -1,13 +1,29 @@
 
 #include "dmdll.h"
+#include "libA.h"
+#include "libB.h"
 
 int main( int argc, char* argv[] ) {
 
-    Idmdll* module = dmdllGetModule();
-    if (module)
+    Idmdll* LibAmodule = dmdllGetModule();
+    Idmdll* LibBmodule = dmdllGetModule();    
+    if (LibAmodule && LibBmodule)
     {
-        module->Test();
-        module->Release();
+        LibAmodule->DMLoadLibrary("liblibA.so");
+
+        LibBmodule->DMLoadLibrary("liblibB.so");
+
+        using libAFunctionType = decltype(libAFunction);
+        using libAFunctionTypeP = libAFunctionType*; 
+        libAFunctionTypeP a = LibAmodule->DMGetProcAddress("libAFunction");
+        a(20);
+        using libBFunctionType = decltype(libBFunction);
+        using libBFunctionTypeP = libBFunctionType*; 
+        libBFunctionTypeP b = LibBmodule->DMGetProcAddress("libBFunction");
+        b(20);
+        
+        LibAmodule->Release();
+        LibBmodule->Release();
     }
     return 0;
 }
